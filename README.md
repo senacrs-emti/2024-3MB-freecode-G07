62efd92f2957cf9b71473699ca4e329468280b7a
Ruda <rudabrandao59@gmail.com>
2024-11-07 01:29:50 -0300

    mapa

--- README.md
+++
@@ -1,1 +0,0 @@
-# 2024-3MB-freecode-G07
---
+++ app/src/main/java/com/example/gps/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.gps
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+    }
+}
